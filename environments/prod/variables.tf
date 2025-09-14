94e83f30ee2170f550e4d2e723c6d75bef260a77
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2025-09-14 18:10:41 +0100

    Don't bother with jellyfin

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -32,6 +32,33 @@ variable "nginx" {
   })
 }
 
+variable "jellyfin" {
+  type = object({
+    app_name = string
+    deployment = object({
+      name = string
+      replicas = number
+    })
+    container = object({
+      name = string
+      image = string
+      port = number
+    })
+    service = object({
+      name = string
+      type = string
+      protocol = string
+      port = number
+      target_port = number
+    }),
+    volumes = list(object({
+      name = string
+      mount_path = string
+      host_path = string
+    }))
+  })
+}
+
 variable "ingress" {
   type = object({
     name = string
@@ -53,50 +80,6 @@ variable "ingress" {
   })
 }
 
-# variable "ingress" {
-#   type = object({
-#     namespace = string
-#     rules = list(object({
-#       host = string
-#       path = string
-#       path_type = string
-#       service = string
-#       port = number
-#     }))
-#   })
-# }
 
-
-# ingress = {
-#   namespace = "production"
-#   rules = [{
-#     host = "web.darigan.tech"
-#     path = "/"
-#     path_type = "Prefix"
-#     service = "nginx-service"
-#     port = 8082
-#   }]
-# }
-
-# resource "helm_release" "caddy_ingress_controller" {
-#   name       = "caddy-ingress-controller"
-#   repository = "https://caddy-ingress-controller.github.io/caddy-ingress"
-#   chart      = "caddy-ingress-controller"
-#   namespace  = "kube-system" # A common namespace for system services
-#   create_namespace = true
-#   atomic = true
-# }
-#
-# variable "caddy_helm_release" {
-#   type = object({
-#     name = string
-#     repository = string
-#     chart = string
-#   })
-# }
-
-# variable ingress {
-#   rules =
-# }
 
 
--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -81,6 +81,33 @@ variable "caddy" {
   })
 }
 
+variable "jellyfin" {
+  type = object({
+    app_name = string
+    deployment = object({
+      name = string
+      replicas = number
+    })
+    container = object({
+      name = string
+      image = string
+      port = number
+    })
+    service = object({
+      name = string
+      type = string
+      protocol = string
+      port = number
+      target_port = number
+    }),
+    volumes = list(object({
+      name = string
+      mount_path = string
+      host_path = string
+    }))
+  })
+}
+
 variable "namespace" {
   type = string
 }
